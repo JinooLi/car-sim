3c9f611c3b920c9506d03996699dc4bef162e28c
jinooLi <jinu1423@naver.com>
2025-08-01 10:50:15 +0000

    feat: add simulate.py

---
+++ simulate.py
@@ -0,0 +1,60 @@
+from src.interface import State, Input, Model, Controller, Visualizer, SimulateResult
+
+
+class Simulator:
+    def __init__(
+        self,
+        model: Model,  # Model to simulate
+        controller: Controller,  # Controller for managing inputs to the model
+        simulation_time: float = 10.0,  # Total simulation time in seconds
+        time_step: float = 0.01,  # Time step for simulation updates
+    ):
+        self.model = model
+        self.controller = controller
+        self.simulation_time = simulation_time
+        self.time_step = time_step
+        self.time_steps = int(simulation_time / time_step)
+        self.control_time_step = controller.controller_time_step
+
+    def simulate(self, initial_state: State) -> SimulateResult:
+        state = initial_state
+        t = 0.0
+        control_time = 0.0
+        result = SimulateResult(self.simulation_time, self.time_step)
+        print(f"Starting simulation with initial position x:{state.x}, y:{state.y}")
+        result.append_state(state)
+        for _ in range(self.time_steps):
+            # Update control signal at specified intervals(self.control_time_step)
+            if t >= control_time:
+                input_signal = self.controller.control(state)
+                control_time += self.control_time_step
+            state = self.__RK4_step(state, input_signal)
+            t += self.time_step
+        print(f"Simulation completed. Final position x:{state.x}, y:{state.y}")
+        return result
+
+    def __RK4_step(self, state: State, input_signal: Input):
+        k1 = self.model.differential(state, input_signal)
+        k2 = self.model.differential(state + k1 * (self.time_step / 2), input_signal)
+        k3 = self.model.differential(state + k2 * (self.time_step / 2), input_signal)
+        k4 = self.model.differential(state + k3 * self.time_step, input_signal)
+        return state + (k1 + 2 * k2 + 2 * k3 + k4) * (self.time_step / 6)
+
+
+if __name__ == "__main__":
+    from src.bicycle_model import (
+        BicycleState,
+        BicycleModel,
+        BicycleController,
+        BicycleVisualizer,
+    )
+
+    model: Model = BicycleModel()
+    controller: Controller = BicycleController(control_time_step=0.01)
+    vis: Visualizer = BicycleVisualizer(fps=30)
+
+    simulator = Simulator(model, controller, simulation_time=10.0, time_step=0.01)
+
+    result = simulator.simulate(BicycleState(x=0.0, y=0.0, theta=0.0, velocity=1.0))
+
+    vis.visualize(result)
